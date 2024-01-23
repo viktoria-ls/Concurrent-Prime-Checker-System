8bd9d5a1385f26a4b041b21f5d49e94845d1ed69
Lila Sicuan <viktoria.sicuan@gmail.com>
2024-01-23 19:32:45 +0800

    Change way of checking if threads are done and updated thread instantiation for loop

--- Main.java
+++ Main.java
@@ -29,33 +29,62 @@ public class Main {
         List<Integer> primes = new ArrayList<Integer>();
         List<Thread> primeThreads = new ArrayList<Thread>();
 
-        int numPerThread = LIMIT/NUM_THREADS;
-        int excess = LIMIT%NUM_THREADS;
-        int tempStart = 0;
-        int tempEnd = 0;
+        // int numPerThread = LIMIT/NUM_THREADS;
+        // int excess = LIMIT%NUM_THREADS;
+        // int tempStart = 0;
+        // int tempEnd = 0;
 
-        for(int i = 2, counter = 1;i <= LIMIT; i++,counter++) {
-            if(counter == 1)
-                tempStart = i;
+        // for(int i = 2, counter = 1;i <= LIMIT; i++,counter++) {
+        //     if(counter == 1)
+        //         tempStart = i;
 
-            if(counter == numPerThread) {
-                counter = 0;
-                tempEnd = i;
-                PrimeRunnable tempRunnable = new PrimeRunnable(tempStart, tempEnd, primes, primeSemaphore, primeLock);
+        //     if(counter == numPerThread) {
+        //         counter = 0;
+        //         tempEnd = i;
+        //         PrimeRunnable tempRunnable = new PrimeRunnable(tempStart, tempEnd, primes, primeSemaphore, primeLock);
                 
-                // Adds to list of Threads and starts newly created one
-                Thread tempThread = new Thread(tempRunnable);
-                primeThreads.add(tempThread);
-                primeThreads.get(primeThreads.size() - 1).start();
-            }
+        //         // Adds to list of Threads and starts newly created one
+        //         Thread tempThread = new Thread(tempRunnable);
+        //         primeThreads.add(tempThread);
+        //         primeThreads.get(primeThreads.size() - 1).start();
+        //     }
+        // }
+
+        int numPerThread = (LIMIT - 2 + 1) / NUM_THREADS;
+        int tempStart = 2;
+        int tempEnd = tempStart + numPerThread - 1;
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            PrimeRunnable tempRunnable = new PrimeRunnable(tempStart, tempEnd, primes, primeSemaphore, primeLock);
+                
+            // Adds to list of Threads and starts newly created one
+            Thread tempThread = new Thread(tempRunnable);
+            primeThreads.add(tempThread);
+            primeThreads.get(primeThreads.size() - 1).start();
+
+            tempStart = tempEnd + 1;
+
+            if (i == NUM_THREADS - 2)
+                tempEnd = LIMIT;
+            else
+                tempEnd += numPerThread;
+            
         }
 
         // Checks if all threads are done (all permits were returned)
-        try {
-            primeSemaphore.acquire(NUM_THREADS);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        // try {
+        //     primeSemaphore.acquire(NUM_THREADS);
+        // } catch (InterruptedException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+        for (Thread thread: primeThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         // Get stop time and total time
